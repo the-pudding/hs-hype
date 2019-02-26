c7cb0d3d85ae8c642083d61223d2cd74d9d430e0
russellgoldenberg <russellgoldenberg@gmail.com>
2019-02-26 15:55:09 -0500

    vid

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,76 +1,110 @@
-import loadData from './load-data'
-import './pudding-chart/flow'
-import EnterView from 'enter-view'
+import loadData from './load-data';
+import './pudding-chart/flow';
+import EnterView from 'enter-view';
 
-let data = []
-let $sel = []
+let data = [];
+let $sel = [];
 
 // selections
-const $charts = d3.selectAll('.chart__flow')
-let charts = {}
+const $charts = d3.selectAll('.chart__flow');
+const charts = {};
 
-let $buttons = d3.selectAll('.chart__meta-replay')
+const $buttons = d3.selectAll('.chart__meta-replay');
 
-function setupCharts(){
-	$sel = d3.select(this)
-	const filter = $sel.attr('data-filter')
+function setupCharts() {
+	$sel = d3.select(this);
+	const filter = $sel.attr('data-filter');
 
-	const big4 = ['University of North Carolina', 'University of Kentucky', 'Duke University', 'University of Kansas']
+	const big4 = [
+		'University of North Carolina',
+		'University of Kentucky',
+		'Duke University',
+		'University of Kansas'
+	];
 
-	let filteredData = null
-	if (filter == "ranked") filteredData = data.filter(d => d.top === 1)
-	if (filter == "top10") filteredData = data.filter(d => d.top === 1 && d.rank <= 10)
-	if (filter == "none") filteredData = data.filter(d => d.top === 0 || (d.top === 1 && d.draft === 1))
-	if (filter == "big4") filteredData = data.filter(d => big4.includes(d.college))
-	if (filter == 'skipCollege') filteredData = data.filter(d => d.coll === 2)
+	let filteredData = null;
+	if (filter == 'ranked') filteredData = data.filter(d => d.top === 1);
+	if (filter == 'top10')
+		filteredData = data.filter(d => d.top === 1 && d.rank <= 10);
+	if (filter == 'none')
+		filteredData = data.filter(
+			d => d.top === 0 || (d.top === 1 && d.draft === 1)
+		);
+	if (filter == 'big4')
+		filteredData = data.filter(d => big4.includes(d.college));
+	if (filter == 'skipCollege') filteredData = data.filter(d => d.coll === 2);
 
-	let indicators = {filteredData, filter}
+	const indicators = { filteredData, filter };
 
-	charts[filter] = $sel
-		.datum(indicators)
-		.createFlow()
-
-
+	charts[filter] = $sel.datum(indicators).createFlow();
 }
 
-function setupFigureEnter(){
+function setupFigureEnter() {
 	EnterView({
 		selector: '.chart__flow',
-		enter: function(el, i){
-			console.log({el})
+		enter(el, i) {
+			console.log({ el });
 			// pause other charts
 			Object.keys(charts).map(d => {
-				const val = charts[d]
-				val.pause()
-			})
-			const fil = d3.select(el).attr('data-filter')
-			const rend = charts[fil]
-			rend.render()
+				const val = charts[d];
+				val.pause();
+			});
+			const fil = d3.select(el).attr('data-filter');
+			const rend = charts[fil];
+			rend.render();
 		},
 		offset: 0.25,
 		once: true
-	})
+	});
 }
 
-function handleButtonClick(){
-	const button = d3.select(this).attr('data-filter')
-	charts[button].render()
+function handleButtonClick() {
+	const button = d3.select(this).attr('data-filter');
+	charts[button].render();
 }
 
 function resize() {
-	Object.keys(charts).forEach(d => charts[d].resize())
-	//charts.each.resize()
+	Object.keys(charts).forEach(d => charts[d].resize());
+	// charts.each.resize()
+}
+
+function handleVidEnter() {
+	const side = d3.select(this).attr('data-side');
+	console.log(side);
+	d3.select(`.intro .prose span[data-side="${side}"]`).classed(
+		'is-selected',
+		true
+	);
+}
+
+function handleSpanEnter() {
+	const side = d3.select(this).attr('data-side');
+	d3.select(`.intro__media[data-side="${side}"]`).classed('is-selected', true);
+}
+
+function setupVideo() {
+	d3.selectAll('.intro__media')
+		.on('mouseenter', handleVidEnter)
+		.on('mouseout', () => {
+			d3.selectAll('.intro .prose span').classed('is-selected', false);
+		});
+	d3.selectAll('.intro .prose span')
+		.on('mouseenter', handleSpanEnter)
+		.on('mouseout', () => {
+			d3.selectAll('.intro__media').classed('is-selected', false);
+		});
 }
 
 function init() {
+	setupVideo();
 	Promise.all([loadData()])
-			.then((results) => {
-				data = results[0]
-				$charts.each(setupCharts)
-				$buttons.on('click', handleButtonClick)
-				setTimeout(setupFigureEnter, 100)
-			})
-			.catch(err => console.log(err))
+		.then(results => {
+			data = results[0];
+			$charts.each(setupCharts);
+			$buttons.on('click', handleButtonClick);
+			setTimeout(setupFigureEnter, 100);
+		})
+		.catch(err => console.log(err));
 }
 
 export default { init, resize };
